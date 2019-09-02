64bc9a29ab8c41fa06835b518a9d5fafa12750e2
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-09-02 19:45:00 +0800

    验证负载均衡方法

--- spring-cloud-alibaba-provider/src/main/java/com/demo/provider/controller/HelloController.java
+++ spring-cloud-alibaba-provider/src/main/java/com/demo/provider/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.demo.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${server.port}")
+    private String port;
     @RequestMapping("hello/{hello}")
     public String hello(@PathVariable(value = "hello") String hello){
-        return hello;
+        return hello+"I'm from "+port;
     }
 }
