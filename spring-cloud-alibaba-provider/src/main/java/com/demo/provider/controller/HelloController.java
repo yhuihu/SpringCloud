045e5d96a7d83a45e807a5a1b45e13778f4ea47b
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-09-05 11:12:40 +0800

    修改为nacos管理配置文件

--- spring-cloud-alibaba-provider/src/main/java/com/demo/provider/controller/HelloController.java
+++ spring-cloud-alibaba-provider/src/main/java/com/demo/provider/controller/HelloController.java
@@ -1,16 +1,23 @@
 package com.demo.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @Value("${server.port}")
-    private String port;
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
     @RequestMapping("hello/{hello}")
     public String hello(@PathVariable(value = "hello") String hello){
-        return hello+"I'm from "+port;
+        return hello;
+    }
+    @GetMapping("/environment")
+    public String test(){
+        return applicationContext.getEnvironment().getProperty("user.name");
     }
 }
